3c3f25cad32959a572a335b50468818231659984
nataliepauchard <natalie.pauchard@gmail.com>
2021-03-06 21:53:15 +1100

    2nd-commit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,23 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
+
+  def create
+    question = Question.create question_params
+    redirect_to question
   end
 
   def edit
   end
 
   def show
+    @question = Question.find params[:id]
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:content, :topic_id)
   end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,10 +13,23 @@ class TopicsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find params[:id]
+  end
+
+  def update
+    topic = Topic.find params[:id]
+    topic.update topic_params
+    redirect_to topic
   end
 
   def show
     @topic = Topic.find params[:id]
+  end
+
+  def destroy
+    topic = Topic.find params[:id]
+    topic.destroy
+    redirect_to topics_path
   end
 
   private
