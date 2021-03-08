278efa298e4b307f634473a3df8537b37d7da1f1
nataliepauchard <natalie.pauchard@gmail.com>
2021-03-08 17:27:10 +1100

    added /questions in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://quiz-app-front-end.herokuapp.com/' #needs to be replaced with an explicit list of real URLs on Heroku when you deploy, you'll get an error, need actual host here # ##
+    origins 'https://quiz-app-front-end.herokuapp.com/questions' #needs to be replaced with an explicit list of real URLs on Heroku when you deploy, you'll get an error, need actual host here # ##
     resource '*',
       :headers => :any,
       :methods => %i( get post put patch delete options head )
